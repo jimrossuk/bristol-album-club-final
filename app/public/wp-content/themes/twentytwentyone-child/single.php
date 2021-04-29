60b3f9e24aa0ca2bcacaaf29b7f03574449c6169
“Jim <jimrossuk@gmail.com>
2021-04-29 11:36:09 +0100

    album size now not changeing shape on responsive

--- app/public/wp-content/themes/twentytwentyone-child/single.php
+++ app/public/wp-content/themes/twentytwentyone-child/single.php
@@ -21,7 +21,7 @@ get_header();
 	<?php
 	$image = get_field('album_cover');
     if( !empty( $image ) ): ?>
-	<img class="album-pic-page" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />			<?php endif; ?>
+	<img class="album-pic-page album-size" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />			<?php endif; ?>
 	
 	<div class="album-data">
 		<p class="date"> <?php the_field('date')  ; ?></p>
@@ -29,12 +29,12 @@ get_header();
 		<h6 class="ave score"> <?php the_field('average_socre')  ; ?></h6>
 		<div class='ave'>
 
-			<p> Club Average:</p>
-			<p > <?php the_field('club_average')  ; ?></p> 
+		<p> Club Average:</p>
+		<h5 > <?php the_field('club_average')  ; ?></h5> 
 		</div>
 		<p class='ave'> Club Max: </p>
-		<p class='ave'><?php the_field('club_max')  ; ?></p>
-		<p class='ave'> Club Minimum: <?php the_field('club_minimum')  ; ?></p>
+		<h5 class='ave'><?php the_field('club_max')  ; ?></h5>
+		<h6 class='ave'> Club Minimum: <?php the_field('club_minimum')  ; ?></h6>
 		<p class='ave r'> Total Votes: <?php the_field('total_votes')  ; ?></p>
 	</div>
 	
